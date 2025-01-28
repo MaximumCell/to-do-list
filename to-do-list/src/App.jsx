d4c3530ea47a756cd1b1b17d59e360e3df7d5556
MaximumCell <alihassanwinner@gmail.com>
2025-01-28 18:52:44 +0500

    adding digtal clock

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,10 +1,12 @@
 import ToDoList from './ToDoList.jsx';
-
+import DigitalClock from './DigitalClock.jsx';
 
 function App() {
   
   return (
     <>
+      <DigitalClock />
+      <br />
       <ToDoList />
     </>
   )
---
+++ to-do-list/src/DigitalClock.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+function DigitalClock() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        },1000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
+    },[]);
+    function formatTime() {
+        
+    }
+    return (
+    <>
+      <div className="clock-container">
+        <div className="clock">
+            <span>00:00:00</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DigitalClock;
